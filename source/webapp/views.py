67c22bac603d8d2df4222faab60a333d06525e68
Said Saliev <repsaid@gmail.com>
2022-11-01 16:12:14 +0600

    add history page and settings for it

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from webapp.game import guess_numbers, secret_nums
 # Create your views here.
+history = {
+    'result': '',
+}
 def index_view(request):
     if request.method == "GET":
         return render(request, "index.html")
@@ -13,4 +16,11 @@ def index_view(request):
         numbers_str = context['numbers'].split()
         context['result'] = guess_numbers(secret_nums, numbers_str)
         print(secret_nums)
+        global history
+        history['result'] += context['result']
         return render(request, 'index.html', context)
+def history_view(request):
+    if request.method == 'GET':
+        global history
+        return render(request, 'history.html', history)
+
