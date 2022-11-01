b36d6fac09fd06e1af44843a7dd7e2aa85cbcb50
Said Saliev <repsaid@gmail.com>
2022-11-01 13:40:14 +0600

    added a view for the post method

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from webapp.game import guess_numbers, secret_nums
 # Create your views here.
 def index_view(request):
     if request.method == "GET":
         return render(request, "index.html")
+
+    elif request.method == "POST":
+        context = {
+            'numbers': request.POST.get('numbers'),
+            'result': '',
+        }
+        numbers_str = context['numbers'].split()
+        context['result'] = guess_numbers(secret_nums, numbers_str)
+        print(secret_nums)
+        return render(request, 'index.html', context)
